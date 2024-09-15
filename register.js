04fcd1d9c3f9eac28f7666f674443bbf16a6fd8e
Sarthak Hirpurkar <159929546+sarthak2863@users.noreply.github.com>
2024-09-15 11:50:31 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getDatabase,set,get,ref,child } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbd6TOi5Vc4D7D7LBV17VtTAxT2R_KkRo",
+  authDomain: "spyfinal-a9e72.firebaseapp.com",
+  projectId: "spyfinal-a9e72",
+  storageBucket: "spyfinal-a9e72.appspot.com",
+  messagingSenderId: "50096583333",
+  appId: "1:50096583333:web:3d74af11f474d28d2d534c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db=getDatabase(app);
+
+document.getElementById("registrationForm").addEventListener('submit', function (e) {
+    e.preventDefault();
+    // window.location.href = 'index.html';
+    set(ref(db, 'user/' + document.getElementById("fullname").value), {
+        fullname: document.getElementById("fullname").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+
+    });
+   
+})
